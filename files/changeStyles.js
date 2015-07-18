68288b9f51e7a7031a8bc120abf41d90b2e8d9fc
vuf <hashed@outlook.jp>
2015-07-18 15:01:53 +0900

    スタイル更新

--- files/changeStyles.js
+++ files/changeStyles.js
@@ -5,6 +5,8 @@
 		'constructor': function() {
 			$('#input_bold, #input_italic, #input_strike, #input_underline').css('display', 'none');
 			$('#text_decorator_frame').css('width', '58px');
+			$('#misc_post_menu').contents().filter(function(){return this.nodeType==3;})[0].textContent = '特殊';
+			$('#misc_post_menu').css('width', '46px');
 			$.each($('#navi_right').children('p'), function() {
 				var caption = $(this).text();
 				$(this).children('img').attr('title', caption);
@@ -14,6 +16,8 @@
 		'destructor': function() {
 			$('#text_decorator_frame').css('width', '138px');
 			$('#input_bold, #input_italic, #input_strike, #input_underline').css('display', 'inline');
+			$('#misc_post_menu').contents().filter(function(){return this.nodeType==3;})[0].textContent = '特殊な投稿';
+			$('#misc_post_menu').css('width', '82px');
 			$.each($('#navi_right').children('p'), function() {
 				var caption = $(this).children('img').attr('title');
 				$(this).children('img').after(caption);
