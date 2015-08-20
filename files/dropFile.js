418078e0d27b69e2c5d4952703978978b859a342
vuf <hashed@outlook.jp>
2015-08-21 04:10:35 +0900

    ドロップできるときは半透明にするように

--- files/dropFile.js
+++ files/dropFile.js
@@ -60,41 +60,48 @@
 				
 				return false;
 			});
-			$('#post_form_single, #post_form_multi').on('drop.dropFile', function(e) {
-				var dataTransfer = e.originalEvent.dataTransfer;
-				
-				if (dataTransfer.files.length != 1) return true;
-				
-				var file = dataTransfer.files[0];
-				
-				var isPicture = (file.type.substr(0, 6).toLowerCase() == 'image/');
-				var maxSize = isPicture ? common.roomInfo.maxPictureFileSize : common.roomInfo.maxFileSize;
-				
-				if (file.size > maxSize) {
-					alert('ファイルサイズが大きすぎます');
+			$('#post_form_single, #post_form_multi')
+				.on('dragenter.dropFile', function(e) {
+					$('#post_form_frame').css('opacity', '0.75');
+				})
+				.on('dragleave.dropFile', function(e) {
+					$('#post_form_frame').css('opacity', '1');
+				})
+				.on('drop.dropFile', function(e) {
+					var dataTransfer = e.originalEvent.dataTransfer;
+					
+					if (dataTransfer.files.length != 1) return true;
+					
+					var file = dataTransfer.files[0];
+					
+					var isPicture = (file.type.substr(0, 6).toLowerCase() == 'image/');
+					var maxSize = isPicture ? common.roomInfo.maxPictureFileSize : common.roomInfo.maxFileSize;
+					
+					if (file.size > maxSize) {
+						alert('ファイルサイズが大きすぎます');
+						return false;
+					}
+					
+					/*
+					// 何故かダメ（return falseを外しても）
+					dataTransfer.clearData();
+					dataTransfer.setData('text/plain', (isPicture ? '[P:*'+'/2]' : '[F:*]'));
+					*/
+					
+					appendText(activeForm, (isPicture ? '[P:*/2]' : '[F:*]'));
+					
+					self.pendingFile = {
+						ready:     true,
+						isPicture: isPicture,
+						file:      file,
+					};
+					
 					return false;
-				}
-				
-				/*
-				// 何故かダメ（return falseを外しても）
-				dataTransfer.clearData();
-				dataTransfer.setData('text/plain', (isPicture ? '[P:*'+'/2]' : '[F:*]'));
-				*/
-				
-				appendText(activeForm, (isPicture ? '[P:*/2]' : '[F:*]'));
-				
-				self.pendingFile = {
-					ready:     true,
-					isPicture: isPicture,
-					file:      file,
-				};
-				
-				return false;
-			});
+				});
 		},
 		'destructor': function() {
 			common.removeFilter('input', 'dropFile');
-			$('#post_form_single, #post_form_multi').off('drop.dropFile');
+			$('#post_form_single, #post_form_multi').off('.dropFile');
 		},
 	};
 });
