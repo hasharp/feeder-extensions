4f494f4870492a21ad7a6265603482f340862eb8
vuf <hashed@outlook.jp>
2015-09-02 01:26:24 +0900

    メッセージをトースト通知した後受信ボックスでもNEWが表示されるように

--- files/toastMessages.js
+++ files/toastMessages.js
@@ -1,6 +1,42 @@
 // 受信したメッセージを右下にトースト表示
 
 (function(self, common, ext, fqon) {
+	// 受信した新着メッセージの数
+	var receivedMessageNum = 0;
+	
+	// 送受信ボックス表示関数書き換え
+	var orgViewMessages = window.viewMessages;
+	var viewMessages = function(mode) {
+		if (mode == 'inbox') {
+			openDialogFrame('受信BOX', {
+				'閉じる': function() {
+					$(this).dialog('close');
+				}
+			});
+			
+			$.get('message.php?mode=' + mode, function(data) {
+				var $data = $(data);
+				
+				// NEW付加
+				var $td = $data.find('#message_view>tbody>tr>td');
+				for (var i=0; i*4<$td.length, i<receivedMessageNum; i++) {
+					$($td[i*4]).html('<img src="img/icons/new.gif" title="新着">');
+				}
+				
+				// カウンタ初期化
+				receivedMessageNum = 0;
+				
+				// 表示
+				$('#message_info').hide();
+				$('#dialog_frame').html($data);
+			});
+		} else {
+			// オリジナルの関数を呼び出す
+			orgViewMessages.call(this, mode);
+		}
+	};
+	
+	// 新着メッセージ受信処理
 	self.syncCallback = function(data) {
 		if (data.code == 6 && data.param == sessionId) {
 			$.get('message.php', {'mode': 'inbox'}, function(data) {
@@ -21,6 +57,9 @@
 						return;
 					}
 					
+					
+					// カウンタ増加
+					receivedMessageNum++;
 					
 					// 情報を取得
 					var name  = content.eq(1).clone().children().remove().end().text();
@@ -47,9 +86,11 @@
 	return {
 		'constructor': function() {
 			socket.on('syncCallback', self.syncCallback);
+			window.viewMessages = viewMessages;
 		},
 		'destructor': function() {
 			socket.removeListener('syncCallback', self.syncCallback);
+			window.viewMessages = orgViewMessages;
 		},
 	};
 });
