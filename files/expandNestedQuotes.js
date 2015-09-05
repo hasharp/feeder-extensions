b483cdf9ad605f269fe593806b4f63a761708791
vuf <hashed@outlook.jp>
2015-09-06 00:22:34 +0900

    引用の取得処理修正

--- files/expandNestedQuotes.js
+++ files/expandNestedQuotes.js
@@ -2,7 +2,7 @@
 
 (function(self, common, ext, fqon) {
 	return {
-		'expand': function(element, id) {
+		'expand': function(element, from, target) {
 			var $element = $(element);
 			var originalHtml = $element.html();
 			
@@ -13,7 +13,7 @@
 			);
 			
 			// ページ番号を取得して
-			$.get('get_page_num.php?mode=0&id=' + id + '&num=20', function(page) {
+			$.get('get_page_num.php?mode=0&id=' + from + '&num=20', function(page) {
 				// エントリリストを取得して
 				$.get('feed_list.php?mode=0&num=20&page=' + page + '&from=0&flip=0', function(rawEntries) {
 					// 適合するエントリを探す
@@ -21,7 +21,7 @@
 					var entryList = rawEntries.split('\n');
 					for (var i=0; i<entryList.length; i++) {
 						var entryId = entryList[i].split('\t')[0];
-						if (entryId == id) {
+						if (entryId == from) {
 							rawEntry = entryList[i];
 						}
 					}
@@ -30,26 +30,30 @@
 						$element.html(originalHtml);
 						return;
 					}
-					// あれば更新
-					var entry = $(arrangeFeed(getFeedArray(rawEntry, false))).find('table.comment>tbody>tr>td[colspan=2]').html();
-					$element.after(entry).remove();
+					// あれば表示
+					var entry = $(arrangeFeed(getFeedArray(rawEntry, false)));
+					var quote = entry.find('span.feed_id:contains('+target+')').parents('table.ref');
+					$element.after(quote).remove();
 				});
 			});
 		},
 		'constructor': function() {
+			// フィルタ関数登録
 			common.addFilter('output', 'expandNestedQuotes', function(entries, skelton) {
 				entries.forEach(function(entry, index, array) {
-					array[index][5] = entry[5].replace(/(<table class="ref">.+?)(&gt;&gt;(\d+))(.+?<\/table>)/g, function(all, prefix, tag, id, suffix) {
-						id = parseInt(id);
-						if (common.roomInfo.latestEntryId > 0 && id > common.roomInfo.latestEntryId) return all;
-						
-						return prefix + '<a onclick="'+fqon+'.expand(this, '+id+')">' + tag + '</a>' + suffix;
+					array[index][5] = entry[5].replace(/(<table class="ref">.+?)(&gt;&gt;(\d+))(.+?<span class="feed_id">(\d+)<\/span>.+?<\/table>)/g, function(all, prefix, tag, target, suffix, from) {
+						// 対象が存在しないIDならリンクにしない
+						target = parseInt(target);
+						if (common.roomInfo.latestEntryId > 0 && target > common.roomInfo.latestEntryId) return all;
+						// リンク化
+						return prefix + '<a onclick="'+fqon+'.expand(this, '+from+', '+target+')">' + tag + '</a>' + suffix;
 					});
 				});
 				return entries;
 			});
 		},
 		'destructor': function() {
+			// フィルタ関数登録解除
 			common.removeFilter('output', 'expandNestedQuotes');
 		},
 	};
